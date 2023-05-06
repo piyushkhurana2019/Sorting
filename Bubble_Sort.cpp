2a4ea3eec64dd5cbb186058a02c0ea2e88876d27
piyushkhurana2019 <96003210+piyushkhurana2019@users.noreply.github.com>
2023-05-06 22:56:09 +0530

    Bubble_Sort.cpp

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -1,54 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int i,j,n,flag;
+    int i, j, n, flag;
 
-    cout<<"enter the size of the array"<<endl;
-    cin>>n;
+    cout << "enter the size of the array" << endl;
+    cin >> n;
 
     int arr[n];
 
-    cout<<"enter the elements of array"<<endl;
+    cout << "enter the elements of array" << endl;
 
-     for ( i = 0; i <n; i++)
-        {
-            cin>>arr[i];
-        }
-     for ( i = 0; i <n; i++)
-        {
-            cout<<arr[i];
-        }
-        cout<<endl;
-
-    //sorting begins here:
-
-    for ( i = 0; i < n-1; i++)
+    for (i = 0; i < n; i++)
     {
-            flag=0;
-        for (j  = 0; j < n-1-i; j++)
-        {
-            if (arr[j+1]<arr[j])
-            {
-                swap(arr[j+1],arr[j]);
-                flag=1;
-            }
-
-        }
-            if (flag==0)
-            {
-                break;
-            }
-
+        cin >> arr[i];
     }
-        
-        cout<<"arr sorted"<<endl;
 
-        for ( i = 0; i <n; i++)
+    cout << "your array before sorting : " << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+
+    // sorting begins here:
+
+    for (i = 0; i < n - 1; i++)
+    {
+        flag = 0;
+        for (j = 0; j < n - 1 - i; j++)
         {
-            cout<<arr[i];
+            if (arr[j + 1] < arr[j])
+            {
+                swap(arr[j + 1], arr[j]);
+                flag = 1;
+            }
         }
-    
-return 0;
+        if (flag == 0)
+        {
+            break;
+        }
+    }
+
+    cout << "arr sorted" << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+
+    return 0;
 }
